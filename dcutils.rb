d9898168046e6c5e3e1633a3eb8778dc57d79067
Emilie Zhang <emilie.m.zhang@gmail.com>
2021-03-26 21:50:12 -0700

    Rename key in dcutils

--- dcutils.rb
+++ dcutils.rb
@@ -2,10 +2,10 @@ require 'blowfish'
 
 module Stegno
     class DCUtils
-        def initialize(coverImg, mode, key, secretImg=nil, secretImgName=nil, outputFile=nil)
+        def initialize(coverImg, mode, blowfishKey, secretImg=nil, secretImgName=nil, outputFile=nil)
             @coverImg = coverImg
             @mode = mode
-            @key = key
+            @blowfishKey = blowfishKey
             @secretImg = secretImg
             @secretImgName = secretImgName
             @outputFile = outputFile
@@ -23,10 +23,10 @@ module Stegno
             secretImgInfo = Stegno::DCMisc.getImgInfo(@secretImgName)
             outputImgInfo = Stegno::DCMisc.getImgInfo(@outputFile)
 
-            key = Blowfish::Key.generate(@key)
-            encrypted_secretImgName = Blowfish.encrypt(secretImgInfo[:name] + "/", key)
+            blowfishKey = Blowfish::Key.generate(@blowfishKey)
+            encrypted_secretImgName = Blowfish.encrypt(secretImgInfo[:name] + "/", blowfishKey)
 
-            dcImage = Stegno::DCImage.new(@coverImg, outputImgInfo[:name], outputImgInfo[:extension], key, @secretImg, encrypted_secretImgName, secretImgInfo[:extension] + "/")
+            dcImage = Stegno::DCImage.new(@coverImg, outputImgInfo[:name], outputImgInfo[:extension], blowfishKey, @secretImg, encrypted_secretImgName, secretImgInfo[:extension] + "/")
             dcImage.encodeSecretFileInfo()
             dcImage.encodeImage()
         end
@@ -34,7 +34,7 @@ module Stegno
         def decode
             outputImgInfo = Stegno::DCMisc.getImgInfo(@outputFile)
 
-            dcImage = Stegno::DCImage.new(@coverImg, outputImgInfo[:name], outputImgInfo[:extension], @key)
+            dcImage = Stegno::DCImage.new(@coverImg, outputImgInfo[:name], outputImgInfo[:extension], @blowfishKey)
             dcImage.decodeSecretFileInfo()
             dcImage.decodeImage()
         end
