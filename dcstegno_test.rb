e55bbdd316dd2d0f8c241b340c0d88c015392f6a
Emilie Zhang <emilie.m.zhang@gmail.com>
2021-03-28 13:53:46 -0700

    Create dcstegno_test.rb

---
+++ dcstegno_test.rb
@@ -0,0 +1,52 @@
+output_formats = [
+    "svg", "tiff", "webp", "bmp", "png"
+]
+
+blowfish_key = "foobar2048"
+caesar_key = 42
+
+puts "Begin Encoding test..."
+
+Dir.foreach('coverImgs') do |coverImg|
+    next if coverImg == '.' or coverImg == '..'
+
+    Dir.foreach('secretImgs') do |secretImg|
+        next if secretImg == '.' or secretImg == '..'
+
+        output_formats.each { |ext|
+            puts "Hiding #{secretImg} in #{coverImg}"
+            
+            cover = coverImg.gsub(/\./, "_")
+            secret = secretImg.gsub(/\./, "_")
+            outputName = "#{secret}-#{cover}.#{ext}"
+
+            system("ruby dcstegno.rb -m encode -c coverImgs/#{coverImg} -o #{outputName} --blowfish #{blowfish_key} --caesar #{caesar_key} -s secretImgs/#{secretImg}")
+            puts
+        } 
+
+    end
+
+end
+
+puts "================================================="
+puts 
+puts "Begin Decoding test..."
+puts 
+
+output_formats.push('jpg')
+output_formats.push('gif')
+
+Dir.foreach('outputs') do |outImg|
+    next if outImg == '.' or outImg == '..'
+
+     output_formats.each { |ext|
+        output = outImg.gsub(/\./, "_")
+        decodeOutputName = "DECODED-#{output}.#{ext}"
+        puts "Decoding #{outImg}"
+
+        system("ruby dcstegno.rb -m decode -c outputs/#{outImg} -o #{decodeOutputName} --blowfish #{blowfish_key} --caesar #{caesar_key}")
+        puts
+    } 
+    puts 
+    puts
+end
